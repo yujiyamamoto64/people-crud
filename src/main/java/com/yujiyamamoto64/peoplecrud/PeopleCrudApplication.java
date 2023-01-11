5d4831dfc207cb36f1e79b4f15dc0fb6d8a3de43
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2023-01-11 09:06:07 -0300

    first commit

---
+++ src/main/java/com/yujiyamamoto64/peoplecrud/PeopleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.peoplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/peoplecrud/PeopleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.peoplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
